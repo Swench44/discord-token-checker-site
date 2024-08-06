797ae45598dc8a7a176d897a962490b5bcc25385
Swench <83586237+GhostTR44@users.noreply.github.com>
2024-08-06 21:57:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
-
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 3000;
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const server = http.createServer(app);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
